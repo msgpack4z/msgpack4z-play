3ae727afff63a92339f71f18f182815dc3aa648f
kenji yoshida <6b656e6a69@gmail.com>
2015-01-17 15:46:45 +0900

    msgpack4z-java07 0.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object build extends Build {
       ("com.typesafe.play" %% "play-json" % "2.3.7") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.0") ::
       ("org.scalacheck" %% "scalacheck" % "1.12.1" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.0" % "test") ::
+      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.1" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.0" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
       Nil
