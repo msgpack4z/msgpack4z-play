627d5fd91e00b6b6adf2527e280d8ad830d843f7
xuwei-k <6b656e6a69@gmail.com>
2015-05-19 01:48:17 +0900

    "msgpack4z-java06" % "0.1.1"

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object build extends Build {
       ("org.scalacheck" %% "scalacheck" % "1.12.2" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.3" % "test").exclude("org.msgpack", "msgpack-core") ::
       ("org.msgpack" % "msgpack-core" % "0.7.0-p9" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.0" % "test") ::
+      ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
       Nil
     )
