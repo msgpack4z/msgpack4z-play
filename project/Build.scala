e3607015ab9a955e2023fb71fd72fe69b57ace3a
xuwei-k <6b656e6a69@gmail.com>
2015-07-05 03:18:32 +0900

    "msgpack4z-java07" % "0.1.4"

--- project/Build.scala
+++ project/Build.scala
@@ -16,8 +16,7 @@ object build extends Build {
     libraryDependencies ++= (
       ("com.typesafe.play" %% "play-json" % "2.4.1") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.3") ::
-      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.3" % "test").exclude("org.msgpack", "msgpack-core") ::
-      ("org.msgpack" % "msgpack-core" % "0.7.0-p9" % "test") ::
+      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.4" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
       Nil
