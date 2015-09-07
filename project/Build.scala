5cbec5da67a8f3947b7aad41a2c884d9d95c8cc5
xuwei-k <6b656e6a69@gmail.com>
2015-09-08 02:28:40 +0900

    play 2.4.3

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object build extends Build {
     name := msgpack4zPlayName,
     scalapropsVersion := "0.1.12",
     libraryDependencies ++= (
-      ("com.typesafe.play" %% "play-json" % "2.4.2") ::
+      ("com.typesafe.play" %% "play-json" % "2.4.3") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.4") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.4" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.1" % "test") ::
