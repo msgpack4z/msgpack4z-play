f58ed1bffc4cadf767b477943545d0d43c138b5f
xuwei-k <6b656e6a69@gmail.com>
2015-11-22 04:29:08 +0900

    Product with Serializable

--- src/main/scala/msgpack4z/UndefindedHandler.scala
+++ src/main/scala/msgpack4z/UndefindedHandler.scala
@@ -1,10 +1,10 @@
 package msgpack4z
 
-sealed abstract class UndefinedHandler(private[msgpack4z] val f: MsgPacker => Unit)
+sealed abstract class UndefinedHandler(private[msgpack4z] val f: MsgPacker => Unit) extends Product with Serializable
 
 object UndefinedHandler {
-  object ConvertNil extends UndefinedHandler(_.packNil())
-  object Ignore extends UndefinedHandler(Function.const(()))
+  case object ConvertNil extends UndefinedHandler(_.packNil())
+  case object Ignore extends UndefinedHandler(Function.const(()))
   final case class ThrowError(error: () => Nothing) extends UndefinedHandler(_ => error())
   final case class ExecuteFunction(func: MsgPacker => Unit) extends UndefinedHandler(func)
   val ThrowSysError: ThrowError = ThrowError(() => sys.error("JsUndefined"))
