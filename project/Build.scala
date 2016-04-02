f9789a436cb01ccfbf9d13a2464bdd59e15c2de8
xuwei-k <6b656e6a69@gmail.com>
2016-04-03 08:14:53 +0900

    scalaprops 0.3.1

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object build extends Build {
     scalapropsSettings
   ).settings(
     name := msgpack4zPlayName,
-    scalapropsVersion := "0.3.0",
+    scalapropsVersion := "0.3.1",
     libraryDependencies ++= (
       ("com.typesafe.play" %% "play-json" % "2.5.0") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.2") ::
