d0336024a99e1bbaf559f9c0b378595187ac0a41
xuwei-k <6b656e6a69@gmail.com>
2016-07-03 15:28:46 +0900

    msgpack4z-core 0.3.3

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object build extends Build {
     scalapropsVersion := "0.3.2",
     libraryDependencies ++= (
       ("com.typesafe.play" %% "play-json" % "2.5.1") ::
-      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.2") ::
+      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.3") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.2.0" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
