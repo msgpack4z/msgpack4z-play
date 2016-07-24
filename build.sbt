e83452f52b90bc152d88a2b32873a1e637ab352a
xuwei-k <6b656e6a69@gmail.com>
2016-07-25 08:21:10 +0900

    sbt 0.13.12

---
+++ build.sbt
@@ -0,0 +1,20 @@
+import build._
+
+Common.settings
+
+scalapropsSettings
+
+name := msgpack4zPlayName
+
+scalapropsVersion := "0.3.2"
+
+libraryDependencies ++= (
+  ("com.typesafe.play" %% "play-json" % "2.5.1") ::
+  ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.3") ::
+  ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.3" % "test") ::
+  ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
+  ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
+  Nil
+)
+
+Sxr.subProjectSxr(Compile, "classes.sxr")
--- project/Build.scala
+++ project/Build.scala
@@ -1,28 +1,6 @@
-import sbt._, Keys._
-import scalaprops.ScalapropsPlugin.autoImport._
+object build {
 
-object build extends Build {
-
-  private val msgpack4zPlayName = "msgpack4z-play"
+  val msgpack4zPlayName = "msgpack4z-play"
   val modules = msgpack4zPlayName :: Nil
-
-  lazy val msgpack4z = Project("msgpack4z-play", file(".")).settings(
-    Common.settings
-  ).settings(
-    scalapropsSettings
-  ).settings(
-    name := msgpack4zPlayName,
-    scalapropsVersion := "0.3.2",
-    libraryDependencies ++= (
-      ("com.typesafe.play" %% "play-json" % "2.5.1") ::
-      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.3") ::
-      ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.3" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
-      ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
-      Nil
-    )
-  ).settings(
-    Sxr.subProjectSxr(Compile, "classes.sxr"): _*
-  )
 
 }
