b3dd002bf14b6067ae607d2d168a8b5e2a855102
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-05-17 11:01:10 +0000

    Update scala-library to 2.13.6

--- project/Common.scala
+++ project/Common.scala
@@ -79,7 +79,7 @@ object Common {
       }
     },
     scalaVersion := Scala212,
-    crossScalaVersions := Scala212 :: "2.13.5" :: "3.0.0" :: Nil,
+    crossScalaVersions := Scala212 :: "2.13.6" :: "3.0.0" :: Nil,
     (Compile / doc / scalacOptions) ++= {
       val tag = tagOrHash.value
       CrossVersion.partialVersion(scalaVersion.value) match {
