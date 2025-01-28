a7ae6a55947ddd291a04eff3005f6bbb4c7862dc
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2025-01-28 08:58:06 +0000

    Update scala3-library, ... to 3.3.5

--- project/Common.scala
+++ project/Common.scala
@@ -80,7 +80,7 @@ object Common {
       }
     },
     scalaVersion := Scala212,
-    crossScalaVersions := Scala212 :: "2.13.16" :: "3.3.4" :: Nil,
+    crossScalaVersions := Scala212 :: "2.13.16" :: "3.3.5" :: Nil,
     (Compile / doc / scalacOptions) ++= {
       val tag = tagOrHash.value
       CrossVersion.partialVersion(scalaVersion.value) match {
