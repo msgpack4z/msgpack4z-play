95af62324546fb7a3a5f105c994706d48d7b6529
kenji yoshida <6b656e6a69@gmail.com>
2026-05-06 16:33:45 +0900

    Remove Sonatype credentials from Common.scala

--- project/Common.scala
+++ project/Common.scala
@@ -53,11 +53,6 @@ object Common {
       UpdateReadme.updateReadmeProcess,
       pushChanges
     ),
-    credentials ++= PartialFunction
-      .condOpt(sys.env.get("SONATYPE_USER") -> sys.env.get("SONATYPE_PASS")) { case (Some(user), Some(pass)) =>
-        Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", user, pass)
-      }
-      .toList,
     organization := "com.github.xuwei-k",
     homepage := Some(url("https://github.com/msgpack4z")),
     licenses := Seq("MIT License" -> url("http://www.opensource.org/licenses/mit-license.php")),
