59fffb95b2ce1b3f812efd85b2638bf05267569a
kenji yoshida <6b656e6a69@gmail.com>
2026-05-06 16:32:44 +0900

    Update project/UpdateReadme.scala

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -4,21 +4,16 @@ import sbtrelease.ReleasePlugin.autoImport.ReleaseStep
 
 object UpdateReadme {
 
-  private val sonatypeURL = "https://oss.sonatype.org/service/local/repositories/"
-
-  val updateReadmeTask = {
-    state: State =>
-      val extracted = Project.extract(state)
-      val scalaV = "2.11"
-      val v = extracted get version
-      val org = extracted get organization
-      val modules = build.modules
-      val snapshotOrRelease = if (extracted get isSnapshot) "snapshots" else "releases"
-      val readme = "README.md"
-      val readmeFile = file(readme)
-      val newReadme = Predef
-        .augmentString(IO.read(readmeFile))
-        .lines
+  val updateReadmeTask: State => State = { state =>
+    val extracted = Project.extract(state)
+    val v = extracted.get(version)
+    val org = extracted.get(organization)
+    val modules = build.modules
+    val snapshotOrRelease = if (extracted.get(isSnapshot)) "snapshots" else "releases"
+    val readme = "README.md"
+    val readmeFile = file(readme)
+    val newReadme =
+      IO.readLines(readmeFile)
         .map { line =>
           val matchReleaseOrSnapshot = line.contains("SNAPSHOT") == v.contains("SNAPSHOT")
           def n = modules(modules.indexWhere(line.contains))
@@ -30,12 +25,12 @@ object UpdateReadme {
           } else line
         }
         .mkString("", "\n", "\n")
-      IO.write(readmeFile, newReadme)
-      val git = new Git(extracted get baseDirectory)
-      git.add(readme) ! state.log
-      git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
-      sys.process.Process("git diff HEAD^") ! state.log
-      state
+    IO.write(readmeFile, newReadme)
+    val git = new Git(extracted.get(baseDirectory))
+    git.add(readme) ! state.log
+    git.commit(message = "update " + readme, sign = false, signOff = false) ! state.log
+    sys.process.Process("git diff HEAD^") ! state.log
+    state
   }
 
   val updateReadmeProcess: ReleaseStep = updateReadmeTask
