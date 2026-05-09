7bc9eeaa4d591882bf66fed75e8ae30929bb3364
xuwei-k <6b656e6a69@gmail.com>
2026-05-10 07:30:19 +0900

    Remove -language:higherKinds

--- project/Common.scala
+++ project/Common.scala
@@ -57,7 +57,6 @@ object Common {
       "-deprecation",
       "-unchecked",
       "-language:existentials",
-      "-language:higherKinds",
       "-language:implicitConversions",
     ),
     scalacOptions ++= {
--- project/plugin.sbt
+++ project/plugin.sbt
@@ -10,7 +10,6 @@ scalacOptions ++= Seq(
   "-deprecation",
   "-unchecked",
   "-language:existentials",
-  "-language:higherKinds",
   "-language:implicitConversions",
   "-Yno-adapted-args",
 )
